534b2fb432696d2c42d6c9540b5067339bb4dc13
Marcin Czajczyk <Marcin.Czajczyk@bottomline.com>
2021-06-10 10:51:49 +0200

    Clear imports

--- jatt-core/src/main/java/com/czajczykmarcin/jatt/core/analyzers/StringFrequencyAnalyzerOne.java
+++ jatt-core/src/main/java/com/czajczykmarcin/jatt/core/analyzers/StringFrequencyAnalyzerOne.java
@@ -13,7 +13,6 @@ import com.czajczykmarcin.jatt.core.response.ResponseImpl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import static com.czajczykmarcin.jatt.core.util.CharacterUtil.caseConverter;
 import static java.util.Objects.requireNonNull;
--- jatt-core/src/main/java/com/czajczykmarcin/jatt/core/analyzers/UnicodeCharacterAnalyzer.java
+++ jatt-core/src/main/java/com/czajczykmarcin/jatt/core/analyzers/UnicodeCharacterAnalyzer.java
@@ -1,8 +1,6 @@
 package com.czajczykmarcin.jatt.core.analyzers;
 
 import com.czajczykmarcin.jatt.core.*;
-import com.czajczykmarcin.jatt.core.exceptions.IllegalCharacter;
-import com.czajczykmarcin.jatt.core.request.CaseMode;
 
 import static com.czajczykmarcin.jatt.core.CharacterAnalyzer.Result.*;
 
